1290f540cb376226228e5d2604155a60ded0c957
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-10-12 18:48:11 +0530

    Create Stats.java

---
+++ src/main/java/statisticker/Stats.java
@@ -0,0 +1,14 @@
+package statisticker; 
+
+public class Stats 
+{
+    private float average;
+    private float min;
+    private float max;
+  
+    public Stats(float average,float min,float max) {
+        this.average = average;
+        this.max = max;
+        this.min = min;
+    }
+}
