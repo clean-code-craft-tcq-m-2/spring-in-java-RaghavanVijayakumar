e0101f15e10831b417ebe0032ae5defefdf8b6ac
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-10-13 09:18:26 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -3,18 +3,51 @@ package statisticker;
 import java.util.List;
 import java.util.Collections;
 
-public class Statistics 
-{
-    float sum=0.0f;
-    public static Stats getStatistics(List<___> numbers) {
-        for(int i:numbers){
-            sum+=i;
-    }
-        average=sum/numbers.size;
-        int length=numbers.size();
-        Collections.sort(numbers);
-         min=numbers.get(0);
-         max=numbers.get(length);
-        return new Stats(average,min,max);
-}
+public class Statistics {
+
+	public static class Stats {
+		public float average;
+		public float min;
+		public float max;
+		boolean checklist;
+		boolean checkavg;
+		boolean checkmin;
+		boolean checkmax;
+
+		public Stats(float average2, float min2, float max2, boolean checklist, boolean checkavg, boolean checkmin,
+				boolean checkmax) {
+			this.average = average2;
+			this.min = min2;
+			this.max = max2;
+			this.checklist = checklist;
+			this.checkavg = checkavg;
+			this.checkmin = checkmin;
+			this.checkmax = checkmax;
+			return;
+		}
+	}
+
+	public static Stats getStatistics(List<FLoat> numbers) {
+		float min = 0, max = 0;
+		float sum = 0;
+		boolean checklist = false;
+		for (Float i : numbers) {
+			sum = sum + i;
+		}
+		int length = numbers.size();
+		float average = sum / length;
+		Collections.sort(numbers);
+		if (numbers.isEmpty()) {
+			checklist = true;
+			min = 0f / 0f;
+			max = 0f / 0f;
+		} else {
+			min = numbers.get(0);
+			max = numbers.get(length - 1);
+		}
+		boolean checkavg = Float.isNaN(average);
+		boolean checkmin = Float.isNaN(max);
+		boolean checkmax = Float.isNaN(min);
+		return new Stats(average, min, max, checklist, checkavg, checkmin, checkmax);
+	}
 }
